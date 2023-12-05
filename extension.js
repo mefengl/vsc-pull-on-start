f1815cec3b6ef3700927f361a9de8e6cfaaec9a0
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-12-05 10:26:34 +0800

    fix: Fix typo and missing semicolons in extension.js

--- extension.js
+++ extension.js
@@ -1,40 +1,50 @@
-const { exec } = require('node:child_process')
-const vscode = require('vscode')
+const { exec } = require('node:child_process');
+const vscode = require('vscode');
 
 function activate() {
-  console.log('Extension "vsc-pull-on-start" is now active!')
+  console.log('Extension "vsc-pull-on-start" is now active!');
 
-  const workspaceFolders = vscode.workspace.workspaceFolders
+  const workspaceFolders = vscode.workspace.workspaceFolders;
   if (workspaceFolders) {
     workspaceFolders.forEach((folder) => {
-      const gitFolderCheckCmd = 'git rev-parse --is-inside-work-tree'
-      const gitRemoteCheckCmd = 'git remote'
-      const options = { cwd: folder.uri.fsPath }
+      const gitFolderCheckCmd = 'git rev-parse --is-inside-work-tree';
+      const gitRemoteCheckCmd = 'git remote';
+      const options = { cwd: folder.uri.fsPath };
 
       exec(gitFolderCheckCmd, options, (error, isGitRepo) => {
         if (error || isGitRepo.trim() !== 'true') {
-          console.log(`${folder.name} is not a Git repository.`)
-          return
+          console.log(`${folder.name} is not a Git repository.`);
+          return;
         }
 
         exec(gitRemoteCheckCmd, options, (error, hasRemote) => {
           if (error || !hasRemote.trim()) {
-            console.log(`No remote found for the Git repository in ${folder.name}.`)
-            return
+            console.log(`No remote found for the Git repository in ${folder.name}.`);
+            return;
           }
 
-          const cmd = 'git pull'
-          exec(cmd, options, (error, stdout) => {
-            if (error) {
-              vscode.window.showErrorMessage(`Error executing git pull in ${folder.name}: ${error}`)
-              return
-            }
-            if (!vscode.workspace.getConfiguration('pullOnStart').get('silence'))
-              vscode.window.showInformationMessage(`Git pull executed in ${folder.name}: ${stdout}`)
-          })
-        })
-      })
-    })
+          const tryPull = (attempt = 1) => {
+            const delay = Math.pow(2, attempt - 1) * 1000; // 2 seconds, 4 seconds, 8 seconds
+            setTimeout(() => {
+              exec('git pull', options, (error, stdout) => {
+                if (error) {
+                  if (attempt < 3) {
+                    tryPull(attempt + 1);
+                  } else {
+                    vscode.window.showErrorMessage(`Error executing git pull in ${folder.name}: ${error}`);
+                  }
+                  return;
+                }
+                if (!vscode.workspace.getConfiguration('pullOnStart').get('silence'))
+                  vscode.window.showInformationMessage(`Git pull executed in ${folder.name}: ${stdout}`);
+              });
+            }, delay);
+          };
+
+          tryPull(); // Initiate the first pull attempt
+        });
+      });
+    });
   }
 }
 
@@ -43,4 +53,4 @@ function deactivate() { }
 module.exports = {
   activate,
   deactivate,
-}
+};
